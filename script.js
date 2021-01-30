e320257f18764d1b42e7593a8082e821e605305e
Sarwar-Putul <sarwarputul@gmail.com>
2021-01-30 21:57:29 +0600

    Submmiting Position

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+//First class calculation
 
         function handleFirstClassChange(isIncrease){
             const firstInput = document.getElementById('first-count');
@@ -6,28 +7,28 @@
             if(isIncrease == true) {
                 firstNewCount = firstCount + 1;
             }
-            if(isIncrease == false) {
+            if(isIncrease == false  && firstCount > 0) {
                 firstNewCount = firstCount - 1;
             }
             firstInput.value = firstNewCount;
             const firstClassTotal = firstNewCount * 150;
             calculateTotal();
         };
-
+// Economy class calculation
         function handleEconomyChange(isIncrease){
             const ecoInput = document.getElementById('eco-count');
             const ecoCount = parseInt(ecoInput.value);
             if(isIncrease == true) {
                 ecoNewCount = ecoCount + 1;
             }
-            if(isIncrease == false) {
+            if(isIncrease == false  && ecoCount > 0) {
                 ecoNewCount = ecoCount - 1;
             }
             ecoInput.value = ecoNewCount;
             const economyTotal = ecoNewCount * 100;
             calculateTotal();
         };
-
+// Final calculation with confirmation slip calculation.
         function calculateTotal() {
             const firstInput = document.getElementById('first-count');
             const firstCount = parseInt(firstInput.value);
